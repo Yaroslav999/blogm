121bcbfbc7f3ab6d8f2a7fe66eaeeb38a9d4ce1e
Yaroslav999 <yara99969@gmail.com>
2018-10-08 12:15:09 +0300

    CRUD Posts & add gems: Bootstrap, Carrierwave

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :set_items, only: [:edit, :destroy, :show, :update]
+
+  def new
+    @post = Post.new
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_index_path
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_items
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:name, :content, :image)
+  end
+end
